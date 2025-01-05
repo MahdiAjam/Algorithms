c85613d99be5472b23ca396bd4e47f206749a17a
MahdiAjam <mahdiajam8754@gmail.com>
2025-01-05 14:04:40 +0330

    the first algorithm <limit>

--- arrays/limit.py
+++ arrays/limit.py
@@ -7,19 +7,13 @@ If array, Min, Max value was given, it returns array that contains values of
  given array which was larger than Min, and lower than Max. You need to give
  'unlimit' to use only Min or Max.
 
-ex) limit([1,2,3,4,5], None, 3) = [1,2,3]
-
 Complexity = O(n)
 """
 
-# tl:dr -- array slicing by value
-def limit(arr, min_lim=None, max_lim=None):
-    if len(arr) == 0:
-        return arr
+def limit(arr, min=None, max=None):
+    min_check = lambda val: True if min is None else (val >= min)
+    max_check = lambda val: True if max is None else (val <= max)
 
-    if min_lim is None:
-        min_lim = min(arr)
-    if max_lim is None:
-        max_lim = max(arr)
+    return [val for val in arr if min_check(val) and max_check(val)]
 
-    return list(filter(lambda x: (min_lim <= x <= max_lim), arr))
+print(limit([1, 2, 3, 4, 5, 6], max=4))
